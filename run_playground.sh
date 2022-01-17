ec271e72edc155447814218e50864a1481169e84
Massimo Tamburini <massimo.tamburini@botika.ai>
2022-01-17 23:03:32 +0100

    revert before merge

--- run_playground.sh
+++ run_playground.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-nix-build -A plutus-playground.client
-nix-shell --run "cd plutus-playground-client && plutus-playground-server &"
-nix-shell --run "build-and-serve-docs &"
+. /home/$USER/.nix-profile/etc/profile.d/nix.sh && nix-build -A plutus-playground.client
+nix-shell --run "cabal update"
+nix-shell --run "cd plutus-playground-client && plutus-playground-server &" 
+nix-shell --run "build-and-serve-docs &" 
 nix-shell --command "cd plutus-playground-client && npm start"
